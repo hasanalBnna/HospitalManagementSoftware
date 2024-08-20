a3a510b0a542b901929bf8a2eeb3ac7cebd02f6b
Kazi Hasan Al Banna <kazi.hasanalbanna59@gmail.com>
2024-08-20 11:13:17 +0600

    1st commit

---
+++ src/main/java/com/hms/model/PathologicalTest.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.hms.model;
+
+/**
+ *
+ * @author Nazmul
+ */
+public class PathologicalTest {
+    String title;
+    double cost;
+    boolean isAvailable;
+    
+    public PathologicalTest(String title,
+                    double cost,
+                    boolean isAvailable){
+        this.title = title;
+        this.cost = cost;
+        this.isAvailable = isAvailable;               
+    }
+     public String show(){
+        String output = "Test name: "+this.title+"<br>"
+                + "Cost: "+this.cost+"<br>"
+                + "Availability: "+this.isAvailable;
+        return output;
+    }
+    void search(String title){
+        if(this.title == title)
+            show();
+        else
+            System.out.println("Not found");
+    }
+}
